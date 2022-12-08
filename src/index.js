9838e0087f90e2f0e9929a37b695459890cc2fb1
PrysiazhnukSerhii <feremiks@gmail.com>
2022-12-08 09:17:35 +0100

    fix app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/welder-resume-Serhii-Prysiazhniuk">
+    <BrowserRouter basename="/welder-resume">
       <App />
     </BrowserRouter>
   </React.StrictMode>
